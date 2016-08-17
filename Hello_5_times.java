e1397c67271097e6cbf4ce8870bb90eeb0d862bd
NaveenaParamasivam <naveenaparamasivam95@gmail.com>
2016-08-17 16:46:28 +0530

    Create Hello_5_times.java

---
+++ Hello_5_times.java
@@ -0,0 +1,12 @@
+package Logical;
+
+public class Hello_5_times 
+{
+	public static void main(String[] args) {
+      String str="Hello";
+      for(int i=0;i<5;i++)
+      {
+    	  System.out.print(str);
+      }
+	}
+}
