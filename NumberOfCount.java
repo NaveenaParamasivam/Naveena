7a2210ca6688a7ff33300083bccd216778849150
NaveenaParamasivam <naveenaparamasivam95@gmail.com>
2016-08-18 20:29:32 +0530

    Create NumberOfCount.java

---
+++ NumberOfCount.java
@@ -0,0 +1,14 @@
+package Ex;
+
+import java.util.Scanner;
+
+public class NumberOfCount 
+{
+  public static void main(String[] args) 
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int a=String.valueOf(n).length();
+    System.out.println(a);
+  }
+}
