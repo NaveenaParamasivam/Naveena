3a3fe4019c4ed0bc140c426d73f3adba49d94c1b
NaveenaParamasivam <naveenaparamasivam95@gmail.com>
2016-09-06 22:17:46 +0530

    Add files via upload

---
+++ String_To_Integer.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class String_To_Integer {
+public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	String st=s.next();
+	int a=Integer.parseInt(st);
+	System.out.println(a);
+}
+}
